d8436a88a654933bb8a11f4ac5a3916d635ee07e
Sharon <slee636@ucsb.edu>
2023-11-19 22:16:48 -0800

    Update sharon_draft.R

--- scripts/sharon_draft.R
+++ scripts/sharon_draft.R
@@ -7,6 +7,7 @@ require(stopwords)
 require(tokenizers)
 
 set.seed(313)
+setwd("/Users/seoeun/UCSB/PSTAT197/module2-f23-module2-group-4")
 load("claims-clean-example.RData")
 
 head(claims_clean)
@@ -17,7 +18,7 @@ partitions <- claims_clean %>%
 
 train_text <- training(partitions) %>%
   pull(text_clean)
-train_labels <- training(partitions) %>%
+train_labels_b <- training(partitions) %>%
   pull(bclass) %>%
   as.numeric()-1
 
@@ -31,7 +32,7 @@ preprocess_layer <- layer_text_vectorization(
 preprocess_layer %>% adapt(train_text)
 
 ## NN
-model <- keras_model_sequential() %>%
+model_b <- keras_model_sequential() %>%
   preprocess_layer() %>%
   layer_dropout(0.2) %>%
   layer_dense(units = 25) %>%
@@ -43,27 +44,33 @@ model <- keras_model_sequential() %>%
   layer_dense(1) %>%
   layer_activation(activation = 'sigmoid')
 
-summary(model)
+summary(model_b)
 
 # configure for training
-model %>% compile(
+model_b %>% compile(
   loss = 'binary_crossentropy',
   optimizer = 'adam',
   metrics = 'binary_accuracy'
 )
 
-history <- model %>%
-  fit(train_text, train_labels, validation_split = 0.3, epochs = 15)
+history_b <- model_b %>%
+  fit(train_text, train_labels_b, validation_split = 0.3, epochs = 15)
 
 ## predict on the testing
 test_text <- testing(partitions) %>%
   pull(text_clean)
-test_pred <- predict(model, test_text)
+test_pred_b <- predict(model_b, test_text) 
 
 ## prediction df
-pred_df_b <- testing(partitions) %>%
-  select(.id) %>%
-  mutate(bclass.pred = (test_pred>0.5))
+class_labels_b <- claims_raw %>% pull(bclass) %>% levels()
+pred_classes_b <- factor(test_pred_b > 0.5, labels = class_labels_b)
+
+pred_classes_b
+
+pred_df <- testing(partitions) %>%
+  bind_cols(bclass.pred = pred_classes_b) %>%
+  select(.id, bclass.pred)
+
 
 ############# multiclass #############
 
@@ -71,6 +78,7 @@ train_labels_m <- training(partitions) %>%
   pull(mclass) %>%
   as.numeric()-1
 
+unique(train_labels_m) # 2 0 4 3 1
 
 model_m <- keras_model_sequential() %>%
   preprocess_layer() %>%
@@ -81,26 +89,28 @@ model_m <- keras_model_sequential() %>%
   layer_dropout(0.2) %>%
   layer_dense(units = 25, activation = 'relu') %>%
   layer_dropout(0.2) %>%
-  layer_dense(1) %>%
+  layer_dense(5) %>%
   layer_activation(activation = 'softmax')
 
 model_m  %>% compile(
-  loss = 'categorical_crossentropy',
+  loss = 'sparse_categorical_crossentropy',
   optimizer = 'adam',
   metrics = 'accuracy'
 )
 
-history_m <- model %>%
+history_m <- model_m %>%
   fit(train_text, train_labels_m, validation_split = 0.3, epochs = 15)
 
 ## predict on the testing
-test_pred_m <- predict(model_m, test_text)  ## keep getting 1 for every row
+test_pred_m <- predict(model_m, test_text)  
+test_pred_m
 
 ## prediction df
-pred_df_m <- testing(partitions) %>%
-  select(.id) %>%
-  mutate(mclass.pred = (test_pred_m>0.5))
+class_labels_m <- claims_raw %>% pull(mclass) %>% levels()
+pred_classes_m <- factor(max.col(test_pred_m), labels = class_labels_m)
 
-## combine into one data frame
-#pred_df <- merge(pred_df_b, pred_df_m, by=".id")
-#pred_df
+pred_df <- pred_df %>%
+  bind_cols(mclass.pred = pred_classes_m)
+pred_df
+
+save(pred_df, file = 'results/example-preds.RData')
